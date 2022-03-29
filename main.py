3b77ae7a119b36c2646a1415dc3894cf61bb4814
Mitchell Kotler <mitch@mitchellkotler.net>
2022-03-29 09:17:18 -0400

    more efficient doc fetching

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ class PdfExport(AddOn):
 
     def main(self):
         with zipfile.ZipFile("export.zip", mode="w") as archive:
-            for doc_id in self.documents:
-                document = self.client.documents.get(doc_id)
+            for document in self.client.documents.list(id__in=self.documents):
                 with archive.open(f"{document.slug} - {document.id}.pdf", "w") as pdf:
                     pdf.write(document.pdf)
 
