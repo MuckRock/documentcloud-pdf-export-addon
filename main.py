6657c81bb20e15990354341d9774fbe4362231b4
Mitchell Kotler <mitch@mitchellkotler.net>
2022-03-30 13:01:41 -0400

    check for at least one document

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ class PdfExport(AddOn):
     """Export all of the selected documents in a zip file"""
 
     def main(self):
+        if not self.documents:
+            self.set_message("Please select at least one document")
+            return
+
         with zipfile.ZipFile("export.zip", mode="w") as archive:
             for document in self.client.documents.list(id__in=self.documents):
                 with archive.open(f"{document.slug} - {document.id}.pdf", "w") as pdf:
