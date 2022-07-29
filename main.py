07109694632afac665dc22f730fcb649b6ea7751
Michael Morisy <m@morisy.com>
2022-07-29 14:49:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,6 @@ class PdfExport(AddOn):
     """Export all of the selected documents in a zip file"""
 
     def main(self):
-        print(self.documents)
-        if not self.get_documents():
-            self.set_message("Please select at least one document")
-            return
-
         with zipfile.ZipFile("export.zip", mode="w") as archive:
             for document in self.get_documents():
                 print(f"{document.slug} - {document.id}.pdf")
