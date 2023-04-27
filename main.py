1d689d3241b6bdf408285e49e9e4cf9108691e96
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-04-27 12:29:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class PdfExport(AddOn):
         if os.path.getsize("export.zip")>5300000000:
             self.set_message("The export you are trying to do is larger than 5GB" 
                              " which is the maximum allowed, please retry "
-                             "on a smaller set of documents."
+                             "on a smaller set of documents.")
         else: 
             self.upload_file(open("export.zip"))
 
