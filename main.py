c57635594a9b8f7614d87b13d74098ea1dec551b
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-04-27 13:08:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 This add-on allows you to bulk export PDFs from DocumentCloud
 """
 import os
+import sys
 import zipfile
 from documentcloud.addon import AddOn
 
@@ -15,13 +16,16 @@ class PdfExport(AddOn):
                 # print(f"{document.slug} - {document.id}.pdf")
                 with archive.open(f"{document.slug} - {document.id}.pdf", "w") as pdf:
                     pdf.write(document.pdf)
+        # If the zip file is >5GB, it won't be able to upload to S3, so need to send a warning. 
         if os.path.getsize("export.zip")>5300000000:
-            self.set_message("The export you are trying to do is larger than 5GB" 
-                             " which is the maximum allowed, please retry "
-                             "on a smaller set of documents.")
+            self.set_message("The export you are trying to do is larger than 5GB " 
+                             "which is the maximum size allowed. Please retry "
+                             "on a smaller set of documents or split up "
+                             "the documents you are trying to export into two runs.")
+            
+            sys.exit(1)
         else: 
             self.upload_file(open("export.zip"))
-
 
 if __name__ == "__main__":
     PdfExport().main()
