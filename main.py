8eab9381cde2536e3a003d5bea8d49209dfe0f37
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2025-02-19 10:54:40 -0600

    Adds timestamp to zip file name

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-"""
-This add-on allows you to bulk export PDFs from DocumentCloud
-"""
 import os
 import sys
 import zipfile
+from datetime import datetime
 from documentcloud.addon import AddOn
 
 
@@ -14,13 +12,19 @@ class PdfExport(AddOn):
         if self.get_document_count() is None:
             self.set_message("Please select at least one document.")
             return
-        with zipfile.ZipFile("export.zip", mode="w") as archive:
+        
+        # Generate timestamp for the zip file (month, day, hour, minute)
+        timestamp = datetime.now().strftime("%m-%d_%H-%M")
+        zip_filename = f"export_{timestamp}.zip"
+        
+        with zipfile.ZipFile(zip_filename, mode="w") as archive:
             for document in self.get_documents():
                 # print(f"{document.slug} - {document.id}.pdf")
                 with archive.open(f"{document.slug} - {document.id}.pdf", "w") as pdf:
                     pdf.write(document.pdf)
+
         # If the zip file is >5GB, it won't be able to upload to S3, so need to send a warning. 
-        if os.path.getsize("export.zip")>5300000000:
+        if os.path.getsize(zip_filename) > 5300000000:
             self.set_message("The export you are trying to do is larger than 5GB " 
                              "which is the maximum size allowed. Please retry "
                              "on a smaller set of documents or split up "
@@ -28,7 +32,7 @@ class PdfExport(AddOn):
             
             sys.exit(1)
         else: 
-            self.upload_file(open("export.zip"))
+            self.upload_file(open(zip_filename))
 
 if __name__ == "__main__":
     PdfExport().main()
